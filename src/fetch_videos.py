dc0b31b2b8288780a2146afffc937d1d390813ec
zhuangliang cao <zhuangliangcao@zhuangliangs-MacBook-Air.local>
2023-12-03 20:25:11 +0000

    webhook half way experiement

---
+++ src/fetch_videos.py
@@ -0,0 +1,90 @@
+import logging
+import sys
+import requests
+import os
+import json
+
+from dotenv import load_dotenv
+from pprint import pformat
+import json
+load_dotenv()
+
+def search_videos(pages_to_search,google_api_key,q,order,search_type,page_token=None):
+    """it invoke search in youtube bigdata v3 api. return the list of specifiy amount of channels
+
+    Parameters: ()
+        pages_to_search (int): Description of how many pages of calling search, 5 result /page
+        google_api_key (str): google credential key for the api
+        q(str): the keyword for searching
+        order (str): option(date/rating/relevance/title/videoCount/viewCount)
+        search_type (str): option(channel/playlist/video)
+        page_token(str): can be used for nextPageToken
+        ref: https://developers.google.com/youtube/v3/docs/search/list?apix_params=%7B%22part%22%3A%5B%22snippet%22%5D%2C%22q%22%3A%22crypto%22%2C%22type%22%3A%5B%22channel%22%5D%7D
+
+    Returns:
+    dict:{'items':[item1,item2]}
+    for each item stands for tailered channel including
+        keys: id,title,publishedAt
+
+   """
+    logging.info('fetch page: 1')
+    payload=fetch_videos_page(google_api_key,q,order,search_type,page_token)
+    all_items=payload['items']
+    for i in range(pages_to_search-1):
+        next_page_token=payload.get('nextPageToken')
+        if next_page_token:
+            logging.info(f'fetch page: {i+2}')
+            payload=fetch_channels_page(google_api_key,q,order,search_type,next_page_token)
+            all_items+=payload['items']
+        else:
+            logging.info('fetch pages stop here as no more')
+    tailerd_payload={'items':
+                    [{
+                    'id': x['id']['channelId'],
+                    'title':x['snippet']['title'],
+                    'publishedAt':x['snippet']['publishedAt'],
+                    } for x in all_items]}
+    return tailerd_payload
+    
+def fetch_videos_page(google_api_key,playlistId,order,search_type,page_token=None):
+    """it invoke search in youtube bigdata v3 api. return single page info
+    return:
+    dict:response payload
+    """
+    response = requests.get(
+        'https://www.googleapis.com/youtube/v3/playlistItems',
+        params={
+            'key': google_api_key,
+            'part':'snippet',
+            'q':q,
+            'order':order,
+            'type':search_type,
+            'page_token':page_token,
+        }  
+    )
+    payload=json.loads(response.text)
+
+    return payload
+
+
+def save_channels(input):
+    """simple save input as json file """
+    with open('./data_example/listof_tailered_channels.json', 'w') as file:
+        json.dump(input, file, indent=4)
+    print('json file done')
+
+def main():
+    logging.info('start')
+    # config：
+    pages_to_search=2
+    google_api_key=os.getenv('google_api_key')
+    playlistId='longUploadsUnspecified'
+    video_list=search_videos(
+        google_api_key,
+        playlistId,
+        )
+    save_channels(video_list)
+
+if __name__=='__main__':
+    logging.basicConfig(level=logging.INFO)
+    sys.exit(main())
--- src/webhook_server.py
+++ src/webhook_server.py
@@ -1,39 +1,32 @@
+import xmltodict
+from xml.parsers.expat import ExpatError
 from flask import Flask, request
 from werkzeug.local import LocalProxy   
+import json
+
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['GET','POST'])
-def webhook():
-    request_context=request
-    challenge=request_context.args.get('hub.challenge')
-    print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>challenge:',challenge)
-    print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>all args:',request_context.args)
-    print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>all body:',request_context)
-    # if request.method == 'POST':
-    # json_response = request.json()
-    # data=request['data']
-    # print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>header:',challenge)
-    if request.method == 'GET':
-        return challenge
-    # args=json_response['args']
-    # print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>data:',data)
-    # print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>args:',args)
-    # print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>args2:',args2)
-    # print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>headers:',json_response['headers'])
-    # print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>headers2:',request_context['headers'])
-    # # elif request.method=='GET':
 
-    # print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>request and type:',request, type(request))
-    # # request_context=request
-    # # param_value=request_context.args.get('hub.challenge')
-    # print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>challenge:',param_value)
+def webhook():
+    challenge = request.args.get('hub.challenge')
+    print('hello here')
+    if challenge:
+        print('hello challenge', challenge)
+        return challenge
+    try:
+        xml_dict = xmltodict.parse(request.data)
+        print(xml_dict)
+        
+        # Save the dictionary to a JSON file
+        # with open('./data_example/notification_feed.json', 'w') as json_file:
+        #     json.dump(xml_dict, json_file,indent=4)
+            
+    except (ExpatError, LookupError):
+        return "", 403
     
+    return "", 204
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5002)
 
-
-
-# from werkzeug.local import LocalProxy   
-# class asdfs(werkzeug.local.LocalProxy)    
-# if request.method == 'POST':
