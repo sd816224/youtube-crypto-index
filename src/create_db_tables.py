d722d5a60b512e0d7c96e9db3057c8633e85b9d7
sd816224 <ufoczl@yahoo.co.uk>
2023-12-04 15:05:53 +0000

    table created

---
+++ src/create_db_tables.py
@@ -0,0 +1,96 @@
+from db_connection import get_connection
+import logging
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+logging.basicConfig()
+logger = logging.getLogger('create_db_tables')
+logger.setLevel(logging.INFO)
+
+def create_tables(conn):
+    # try:
+    conn.run(f"""
+                      CREATE SCHEMA IF NOT EXISTS yt;
+                      """)
+    logger.info('create schema')
+    
+    conn.run(f"""
+                      CREATE TABLE IF NOT EXISTS yt.watch_channels(
+                        id INT PRIMARY KEY NOT NULL,
+                        channel_id VARCHAR NOT NULL,
+                        uploads_id VARCHAR NOT NULL,
+                        title VARCHAR NOT NULL,
+                        published_at TIMESTAMP NOT NULL,
+                        country VARCHAR NOT NULL,
+                        statistic_id INT NOT NULL,
+                        status_id INT NOT NULL
+                      );
+                      CREATE TABLE IF NOT EXISTS yt.statistics(
+                        id SERIAL PRIMARY KEY,
+                        view_count INT NOT NULL,
+                        subscriber_count INT NOT NULL,
+                        hidden_subscriber_count BOOLEAN NOT NULL,
+                        video_count INT NOT NULL
+                      );
+                      CREATE TABLE IF NOT EXISTS yt.status(
+                        id INT PRIMARY KEY NOT NULL,
+                        privacy_status VARCHAR NOT NULL,
+                        is_linked BOOLEAN NOT NULL,
+                        long_uploads_status VARCHAR NOT NULL
+                      );
+                      """)
+    logger.info('create table: yt.watch_channels/statistics/status  [column_name,dadta_type]')
+
+    result=conn.run(f"""
+                        SELECT column_name,data_type FROM information_schema.columns
+                        WHERE table_schema = 'yt'
+                        AND table_name = 'watch_channels';
+                """)
+    logger.info(result)
+    result=conn.run(f"""
+                        SELECT column_name,data_type FROM information_schema.columns
+                        WHERE table_schema = 'yt'
+                        AND table_name = 'statistics';
+                """)
+    logger.info(result)
+    result=conn.run(f"""
+                        SELECT column_name,data_type FROM information_schema.columns
+                        WHERE table_schema = 'yt'
+                        AND table_name = 'status';
+                """)
+    logger.info(result)
+
+
+def check_table(conn):
+    result=conn.run(f"""
+                        SELECT * FROM information_schema.tables 
+                        WHERE table_schema = 'yt'
+                """)
+    logger.info('yt.watch_channels: [column_name,dadta_type]')
+    logger.info(result)
+
+def destroy_tables(conn):
+    conn.run('DROP TABLE IF EXISTS yt.watch_channels;')
+    logger.info('destroy yt.watch_channels table')
+    # result = conn.run('DROP SCHEMA IF EXISTS yt;')
+    # logger.info('destroy yt schema')
+
+
+
+
+conn=get_connection(
+    {
+        'RDS_USERNAME': os.getenv('RDS_USERNAME'),
+        'RDS_HOSTNAME': os.getenv('RDS_HOSTNAME'),
+        'DS_DB_NAME': os.getenv('DS_DB_NAME'),
+        'RDS_PORT': os.getenv('RDS_PORT'),
+        'RDS_PASSWORD': os.getenv('RDS_PASSWORD'),
+    }
+)
+# create_tables(conn)
+check_table(conn)
+# destroy_tables(conn)
+
+
+conn.close()
