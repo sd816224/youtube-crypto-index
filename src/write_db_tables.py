d0f59fd7ea9820c0654d9d86d3a6426398467f0f
sd816224 <ufoczl@yahoo.co.uk>
2023-12-05 16:53:24 +0000

    write_table . channels_list done

--- src/write_db_tables.py
+++ src/write_db_tables.py
@@ -4,17 +4,19 @@ from pprint import pprint
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  
+load_dotenv()
+
 
 def read_json_file(file_path):
     with open(file_path, 'r') as file:
         data = json.load(file)
     return data
 
-output_json_file_name='./data_example/ready_channel_list.json'
-content=read_json_file(output_json_file_name)
+
+output_json_file_name = './data_example/ready_channel_list.json'
+content = read_json_file(output_json_file_name)
 pprint(content)
-channel_id='UCi7RBPfTtRkVchV6qO8PUzg'
+channel_id = 'UCi7RBPfTtRkVchV6qO8PUzg'
 conn = get_connection(
     {
         'RDS_USERNAME': os.getenv('RDS_USERNAME'),
@@ -25,17 +27,20 @@ conn = get_connection(
     }
 )
 
-def wirte_channels_table_db(conn,channels_content):
-    """ query database. 
+
+def wirte_channels_table_db(conn, channels_content):
+    """ query database.
     insert ready_channel_list into yt.watch_channels
     """
     cursor = conn.cursor()
-    query ='INSERT INTO yt.watch_channels (channel_id, uploads_id, title, published_at, country) VALUES (%s, %s, %s, %s, %s)'
-    data=[
-        (x['id'],x['uploads_id'],x['title'],x['publishedAt'],x['country']) for x in channels_content
-    ]
+    query = 'INSERT INTO yt.watch_channels (channel_id, uploads_id, title, published_at, country) VALUES (%s, %s, %s, %s, %s)' # noqa E501
+    data = [
+        (x['id'],
+         x['uploads_id'],
+            x['title'],
+            x['publishedAt'],
+            x['country']) for x in channels_content]
     cursor.executemany(query, data)
     conn.commit()
     cursor.close()
     conn.close()
-
