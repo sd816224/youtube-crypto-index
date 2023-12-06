f110d40d9b556cb770040af882db1f64f7bd7808
sd816224 <ufoczl@yahoo.co.uk>
2023-12-06 15:47:11 +0000

    fetch_videos done

--- src/fetch_videos.py
+++ src/fetch_videos.py
@@ -1,18 +1,13 @@
 import logging
-import sys
 import requests
-import os
 import json
 
-from dotenv import load_dotenv
-from pprint import pprint
-import json
-load_dotenv()
 logging.basicConfig()
 logger = logging.getLogger('fetch_videos')
 logger.setLevel(logging.INFO)
 
-def fetch_videos(google_api_key,playlistId,maxResults,page_token=None):
+
+def fetch_videos(google_api_key, playlistId, maxResults, page_token=None):
     """it invoke listitems in youtube bigdata v3 api. return all videos in the list
 
     Parameters: ()
@@ -27,49 +22,64 @@ def fetch_videos(google_api_key,playlistId,maxResults,page_token=None):
     for each item stands for tailered channel including
         keys: id,title,publishedAt
 
-   """
+   """ # noqa E501
     logger.info('fetch page: 1')
-    payload=fetch_videos_page(google_api_key,playlistId,maxResults,page_token)
-    next_page_token=payload.get('nextPageToken')
-    all_items=payload['items']
-    page_no=2
+    payload = fetch_videos_page(
+        google_api_key,
+        playlistId,
+        maxResults,
+        page_token)
+    next_page_token = payload.get('nextPageToken')
+    all_items = payload['items']
+    page_no = 2
+    print(next_page_token)
     while next_page_token:
         logger.info(f'fetch page: {page_no}')
-        payload=fetch_videos_page(google_api_key,playlistId,maxResults,next_page_token)
-        next_page_token=payload.get('nextPageToken')
-        all_items+=payload['items']
-        page_no+=1
+        payload = fetch_videos_page(
+            google_api_key,
+            playlistId,
+            maxResults,
+            next_page_token)
+        next_page_token = payload.get('nextPageToken')
+        all_items += payload['items']
+        page_no += 1
 
-    tailerd_payload={'items':
-                    [{
-                    'id': item['id'],
-                    'title':item['snippet']['title'],
-                    'videoPublishedAt':item['contentDetails']['videoPublishedAt'],
-                    'videoId':item['snippet']['resourceId']['videoId'],
-                    'channel_id':item['snippet']['channelId'],
-                    # 'description':item['snippet']['description'],
-                    } for item in all_items]}
+    tailerd_payload = {'items':
+                       [{
+                        'id': item['id'],
+                        'title': item['snippet']['title'],
+                        'videoPublishedAt': item['contentDetails']['videoPublishedAt'],# noqa E501
+                        'videoId': item['snippet']['resourceId']['videoId'],
+                        'channel_id': item['snippet']['channelId'],
+                        # 'description':item['snippet']['description'],
+                        } for item in all_items]}
     return tailerd_payload
-    
-def fetch_videos_page(google_api_key,playlistId,maxResults,pageToken=None):
+
+
+def fetch_videos_page(google_api_key, playlistId, maxResults, pageToken=None):
     """it invoke search in youtube bigdata v3 api. return single page info
     return:
     dict:response payload
     """
-    response = requests.get(
-        'https://www.googleapis.com/youtube/v3/playlistItems',
-        params={
-            'key': google_api_key,
-            'part':'snippet,id,contentDetails',
-            'playlistId':playlistId,
-            'maxResults':maxResults,
-            'pageToken':pageToken,
-        }  
-    )
-    payload=json.loads(response.text)
-
-    return payload
-
+    try:
+        response = requests.get(
+            'https://www.googleapis.com/youtube/v3/playlistItems',
+            params={
+                'key': google_api_key,
+                'part': 'snippet,id,contentDetails',
+                'playlistId': playlistId,
+                'maxResults': maxResults,
+                'pageToken': pageToken,
+            }
+        )
+        payload = json.loads(response.text)
+        if 'error' in payload:
+            logger.error('error in response: fetch_videos_page')
+            return None
+        return payload
+    except Exception as e:
+        logger.error(e)
+        return None
 
 # def save_channels(input):
 #     """simple save input as json file """
@@ -79,7 +89,6 @@ def fetch_videos_page(google_api_key,playlistId,maxResults,pageToken=None):
 
 # google_api_key=os.getenv('google_api_key')
 # playlistId='UU61jC9ggxeGu8HJ9Q_TxOGg'
-# maxResults='50'
-# videos=fetch_videos(google_api_key,playlistId,maxResults)
-# save_channels(videos)
-# pprint(len(videos['items']))
+# maxResults='3'
+# videos=fetch_videos_page(google_api_key,playlistId,maxResults)
+# pprint(videos)
