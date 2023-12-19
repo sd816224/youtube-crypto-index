7bb7928958517a5b5e9fa8f7228f807f28118995
sd816224 <ufoczl@yahoo.co.uk>
2023-12-19 17:10:50 +0000

    display full test

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,15 +1,15 @@
 from dash import Dash,html
-# from dash import dcc
-# from dash import Input, Output
+from dash import dcc
+from dash import Input, Output
 # import dash_bootstrap_components as dbc
 # import plotly.express as px
-# import plotly.graph_objects as go
+import plotly.graph_objects as go
 from db_connection import get_connection
 
 
-# import pandas as pd
+import pandas as pd
 # import numpy as np
-# import requests
+import requests
 from dotenv import load_dotenv
 import os
 load_dotenv()
@@ -17,10 +17,10 @@ load_dotenv()
 app = Dash()
 app.layout = html.Div([
     html.H1('Youtube-Crypto-Index'),
-    # html.Div(id="new_video_container"),
-    # dcc.Graph(id='candles'),
-    # dcc.Interval(id='page_refresh_interval', interval=2000),
-    # html.H1(id='count_up'),
+    html.Div(id="new_video_container"),
+    dcc.Graph(id='candles'),
+    dcc.Interval(id='page_refresh_interval', interval=2000),
+    html.H1(id='count_up'),
 ])
 conn = get_connection(
     {
@@ -42,51 +42,51 @@ def query_latest_videos(conn):
     return titles, published_at, video_id
 
 
-# @app.callback(
-#     Output('candles', 'figure'),
-#     Output('count_up', 'children'),
-#     Output('new_video_container', 'children'),
-#     Input('page_refresh_interval', 'n_intervals')
-# )
-# def update_figure(n_intervals):
+@app.callback(
+    Output('candles', 'figure'),
+    Output('count_up', 'children'),
+    Output('new_video_container', 'children'),
+    Input('page_refresh_interval', 'n_intervals')
+)
+def update_figure(n_intervals):
 
-#     url = 'https://www.bitstamp.net/api/v2/ohlc/btcusd/'
-#     params = {
-#         'step': '60',
-#         'limit': '30',
-#     }
-#     try:
+    url = 'https://www.bitstamp.net/api/v2/ohlc/btcusd/'
+    params = {
+        'step': '60',
+        'limit': '30',
+    }
+    try:
 
-#         data = requests.get(url, params=params).json()['data']['ohlc']
-#         data = pd.DataFrame(
-#             data,
-#             columns=[
-#                 'timestamp',
-#                 'open',
-#                 'high',
-#                 'low',
-#                 'close'])
-#         data.timestamp = data.timestamp.astype(int)
-#         data.timestamp = pd.to_datetime(data.timestamp, unit='s')
-#         candles = go.Figure(
-#             data=[
-#                 go.Candlestick(
-#                     x=data.timestamp,
-#                     open=data.open,
-#                     high=data.high,
-#                     low=data.low,
-#                     close=data.close,
-#                 )])
-#         titles, published_at, video_id = query_latest_videos(conn)
-#         video_links = [html.A(video_id, href=f"https://www.youtube.com/watch?v={video_id}", target="_blank") for video_id in video_id]  # noqa
-#         new_video_table = html.Table([
-#         html.Caption("New uploaded Videos", style={"font-size": "28px", "font-weight": "bold"}),  # noqa
-#         html.Thead(html.Tr([html.Th("published_at"), html.Th("titles"), html.Th("video_links")])),  # noqa
-#         html.Tbody([html.Tr([html.Td(a), html.Td(b), html.Td(c)]) for a, b, c in zip(published_at, titles, video_links)])])# noqa
-#     except Exception as e:
-#         print(e)
+        data = requests.get(url, params=params).json()['data']['ohlc']
+        data = pd.DataFrame(
+            data,
+            columns=[
+                'timestamp',
+                'open',
+                'high',
+                'low',
+                'close'])
+        data.timestamp = data.timestamp.astype(int)
+        data.timestamp = pd.to_datetime(data.timestamp, unit='s')
+        candles = go.Figure(
+            data=[
+                go.Candlestick(
+                    x=data.timestamp,
+                    open=data.open,
+                    high=data.high,
+                    low=data.low,
+                    close=data.close,
+                )])
+        titles, published_at, video_id = query_latest_videos(conn)
+        video_links = [html.A(video_id, href=f"https://www.youtube.com/watch?v={video_id}", target="_blank") for video_id in video_id]  # noqa
+        new_video_table = html.Table([
+        html.Caption("New uploaded Videos", style={"font-size": "28px", "font-weight": "bold"}),  # noqa
+        html.Thead(html.Tr([html.Th("published_at"), html.Th("titles"), html.Th("video_links")])),  # noqa
+        html.Tbody([html.Tr([html.Td(a), html.Td(b), html.Td(c)]) for a, b, c in zip(published_at, titles, video_links)])])# noqa
+    except Exception as e:
+        print(e)
 
-#     return candles, n_intervals, new_video_table
+    return candles, n_intervals, new_video_table
 
 
 if __name__ == '__main__':
