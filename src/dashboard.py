98d94a7b4f04097241e095ab651c9383ab85371b
sd816224 <ufoczl@yahoo.co.uk>
2023-12-21 23:43:00 +0000

    adjust title

--- src/dashboard.py
+++ src/dashboard.py
@@ -91,11 +91,10 @@ tblcols = [
 ]
 
 app_dash.layout = html.Div([
-    html.H1('Youtube-Crypto-Index (YCI)',style={"font-size": "65px", "font-weight": "bold"}),
-     # noqa
+    html.H1('Youtube-Crypto-Index (YCI)', style={"font-size": "65px", "font-weight": "bold"}), # noqa
     dbc.Row(
         dbc.Col(
-            html.H2("Latest cryptocurrency-related videos",style={"font-size": "28px", "font-weight": "bold"}), 
+            html.H2("Latest cryptocurrency-related videos", style={"font-size": "28px", "font-weight": "bold"}), # noqa
             width={"offset": 3},
         )
     ),
